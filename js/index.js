68273e35bd4a5283e686e721ba1e9960d15c60b8
m_borisova <m.borisova@upz.ru>
2024-11-19 14:29:41 +0500

    черновой вариант сладера и боковых панелей с видео

---
+++ js/index.js
@@ -0,0 +1,70 @@
+﻿const body = document.querySelector('body');
+let themeButton = document.querySelector('.change-theme');
+let themeIcon = document.querySelector('.change-theme');
+let homeBtn = document.querySelector('.name-page');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'light') {
+        body.classList.add('light');
+        themeIcon.src ='icons/dark-theme.svg';
+    } else {
+        body.classList.remove('light');
+        themeIcon.src ='icons/light-theme.svg';
+    }
+});
+
+themeButton.onclick = function () {
+    body.classList.toggle('light');
+    let changedTheme = body.classList.contains('light') ? 'light' : 'dark';
+    localStorage.setItem('theme', changedTheme);
+    themeIcon.src = changedTheme === 'light' ? 'icons/dark-theme.svg' : 'icons/light-theme.svg';
+}
+
+homeBtn.addEventListener('click', function(){
+    location.reload();
+});
+
+document.querySelectorAll('.openVideos').forEach(button => {
+    button.addEventListener('click', () => {
+        let buttonImg = button.querySelector('img');
+        let videosContainer = button.parentElement.querySelector('.home-page_videos');
+
+        if (videosContainer.style.width === '95%') {
+            videosContainer.style.width = '0';
+            button.style.width = '60px';
+            if (buttonImg.classList.contains('chevron-right')) {
+                buttonImg.classList.remove('chevron-right');
+                buttonImg.classList.add('chevron-left');
+            } else if (buttonImg.classList.contains('chevron-left')) {
+                buttonImg.classList.remove('chevron-left');
+                buttonImg.classList.add('chevron-right');  
+            }
+        } else {
+            videosContainer.style.width = '95%';
+            button.style.width = '75px';
+            if (buttonImg.classList.contains('chevron-right')) {
+                buttonImg.classList.remove('chevron-right');
+                buttonImg.classList.add('chevron-left');
+            } else if (buttonImg.classList.contains('chevron-left')) {
+                buttonImg.classList.remove('chevron-left');
+                buttonImg.classList.add('chevron-right');
+            }
+        }
+    });
+});
+
+let currentSlide = 0;
+function changeSlide(direction) {
+    const slides = document.querySelectorAll('.slide');
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    } else if (currentSlide >= slides.length) {
+        currentSlide = 0; 
+    }
+
+    const offset = -currentSlide * 100; // Вычисляем смещение
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
---
+++ js/main.js
@@ -0,0 +1,32 @@
+﻿const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            name1: 'HOME',
+            name2: 'ABOUT',
+            name3: 'MODELS',
+            showAboutPanel: false,
+            showModelsPanel: false,
+            showScrim: false,
+            slides: '<div class="slide"><img src="icons/model1.png"><p><span>DUCATI</span> Panigale V4 2020</p></div> <div class="slide"><img src="icons/model2.png"><p><span>DUCATI</span> Superleggera V4 2020</p></div> <div class="slide"><img src="icons/model3.png"><p><span>DUCATI</span> SuperSport S 2021</p></div> <div class="slide"><img src="icons/model4.png"><p><span>DUCATI</span> Panigale V4 SP2 2022</p></div>'
+        };
+    },
+    methods: {
+        aboutPanel() {
+            this.showAboutPanel = true;
+            this.showModelsPanel = false;
+            this.showScrim = true;
+        },
+        modelsPanel() {
+            this.showAboutPanel = false;
+            this.showModelsPanel = true;
+            this.showScrim = true;
+        },
+        closePanel(){
+            this.showAboutPanel = false;
+            this.showModelsPanel = false;
+            this.showScrim = false;
+        }
+    }
+}).mount(document.body)
